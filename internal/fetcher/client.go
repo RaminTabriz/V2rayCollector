295c1f79a2319b4e70e0d143f633457a3a3c8335
ramin00542 <39794336+ramin00542@users.noreply.github.com>
2026-05-10 16:18:16 +0330

    Update client.go

--- internal/fetcher/client.go
+++ internal/fetcher/client.go
@@ -4,11 +4,15 @@ import (
     "crypto/tls"
     "net"
     "net/http"
+    "net/url"
+    "os"
     "time"
 )
 
 var Client *http.Client
+var proxyURL string // می‌توان از flag یا env مقداردهی کرد
 
+// Init مقداردهی اولیه HTTP client با تنظیمات timeout و proxy
 func Init() {
     transport := &http.Transport{
         TLSClientConfig: &tls.Config{InsecureSkipVerify: false},
@@ -20,13 +24,34 @@ func Init() {
         IdleConnTimeout:     90 * time.Second,
         TLSHandshakeTimeout: 10 * time.Second,
     }
-    // Proxy support from env
+
+    // پشتیبانی از proxy از محیط یا flag
+    if proxyURL == "" {
+        proxyURL = os.Getenv("HTTP_PROXY")
+        if proxyURL == "" {
+            proxyURL = os.Getenv("HTTPS_PROXY")
+        }
+    }
+    if proxyURL != "" {
+        if proxy, err := url.Parse(proxyURL); err == nil {
+            transport.Proxy = http.ProxyURL(proxy)
+        }
+    }
+
     Client = &http.Client{
         Timeout:   30 * time.Second,
         Transport: transport,
     }
 }
 
+// SetProxy تنظیم دستی proxy (اختیاری)
+func SetProxy(proxy string) {
+    proxyURL = proxy
+    // برای اعمال تغییر، باید دوباره مقداردهی کنیم
+    Init()
+}
+
+// Close بستن اتصالات بیکار client
 func Close() {
     if Client != nil {
         Client.CloseIdleConnections()
